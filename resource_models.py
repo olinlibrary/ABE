2d99aafc244f86dbe0a703b5f28fbe4545b1c6fd
Elepert <emily.lepert@students.olin.edu>
2017-07-10 11:05:07 -0400

    updated debug statement

--- resource_models.py
+++ resource_models.py
@@ -30,7 +30,8 @@ class EventApi(Resource):
 
     def get(self, event_id=None, rec_id=None):
         """Retrieve events"""
-        logging.debug("showing entire database {}".format(db.Event.find({})))
+        for i in db.Event.objects({}):
+            logging.debug("showing entire database {}".format(mongo_to_dict(i))
         if event_id:  # use event id if present
             logging.debug('Event requested: ' + event_id)
             result = db.Event.objects(id=event_id).first()
