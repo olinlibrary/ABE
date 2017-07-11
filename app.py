7347a40f894d1eff76a03ad42b4e92224b79c0de
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2017-07-11 16:44:36 -0400

    Fix debug env checking

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def add_label():
 
 
 if __name__ == '__main__':
-    app.debug = os.getenv('FLASK_DEBUG', True) # updates the page as the code is saved
+    app.debug = os.getenv('FLASK_DEBUG') != 'False'  # updates the page as the code is saved
     HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT)
