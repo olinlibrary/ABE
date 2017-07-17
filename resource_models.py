50f95fa7d92120bb8339d47c8818d857a0e8f05f
Elepert <emily.lepert@students.olin.edu>
2017-07-17 16:44:22 -0400

    periodic update of ics feeds works locally

--- resource_models.py
+++ resource_models.py
@@ -76,13 +76,13 @@ class EventApi(Resource):
 
                 # checks for recurrent events
                 if 'recurrence' in event:
-                    logging.debug("recurrence: {}".format(mongo_to_dict(event)))
+                    #logging.debug("recurrence: {}".format(mongo_to_dict(event)))
                     # checks for events from a recurrence that's been edited
                     events_list = recurring_to_full(event, events_list, start, end)
                 else:
-                    logging.debug("normal: {}".format(mongo_to_dict(event)))
-                    #events_list.append(mongo_to_dict(event))
-            logging.debug("events_list: {}".format(events_list))
+                    #logging.debug("normal: {}".format(mongo_to_dict(event)))
+                    events_list.append(mongo_to_dict(event))
+            #logging.debug("events_list: {}".format(events_list))
             return events_list
 
     def post(self):
@@ -246,7 +246,7 @@ class ICSFeed(Resource):
         else:
             labels = ['unlabeled']
 
-        extract_ics(cal, labels, url['url'])
+        extract_ics(cal, url['url'], labels)
         
 
     def put(self, ics_name):
